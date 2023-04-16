dbddfb9bf741b63916ba4c527e18b434e977d435
Zakhriddinov <zaxriddinovsardor21@gmail.com>
2023-04-16 17:24:52 +0500

    navbar view hbs

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,9 +1,19 @@
+const { Product } = require("../models/productModel");
+const asyncHandler = require("express-async-handler");
+
 // @route GET api/products
 // @desc Get all products
 // @access Public
-
 const getProducts = asyncHandler(async (req, res) => {
+   try {
+      const products = await Product.find({});
 
+      return res.status(200).json({
+         products
+      })
+   } catch (error) {
+      console.log(error);
+   }
 })
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,10 +1,39 @@
 const express = require("express");
-require("dotenv").config();
+const connectDB = require("./config/db");
+const { engine } = require("express-handlebars");
+const dotenv = require("dotenv");
+const path = require("path");
+require("colors");
+
+// Env variables
+dotenv.config()
+
+// Connecting to database
+connectDB();
 
 const app = express();
 
-// routes:
-app.use("/api/v1/products", require("./routes/productRoutes"));
+// Body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, './public')));
+
+// Initialize template engine (Handlebars)
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+   res.render('home', {
+      title: "salom"
+   })
+})
+
+
+// Initial routes
+// app.use("/api/v1/products", require("./routes/productRoutes"));
 
 // server started
 const PORT = process.env.PORT || 5000;
